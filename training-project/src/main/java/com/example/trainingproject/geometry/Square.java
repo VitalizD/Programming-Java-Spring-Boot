9d1e006cc01ce82d99bba1352e70b539397ad0db
Виталий Дмитриев <dmitriz_vitaliy@mail.ru>
2022-03-24 14:19:15 +0500

    commit-1

---
+++ training-project/src/main/java/com/example/trainingproject/geometry/Square.java
@@ -0,0 +1,11 @@
+package com.example.trainingproject.geometry;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Square {
+
+    public String info() {
+        return "This is square. ";
+    }
+}
