2d4681f354944f4ae6ce6012cf7cc80ffac6d96f
Виталий Дмитриев <dmitriz_vitaliy@mail.ru>
2022-04-11 22:19:08 +0500

    commit-1

---
+++ security/src/main/java/com/example/security/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.security.service;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public User getUser() {
+        return (User)SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+    }
+
+    public String getUsernameAndAuthorities() {
+        var user = getUser();
+        return "Username: " + user.getUsername() + "\n" +
+                "Authorities: " + user.getAuthorities();
+    }
+}
